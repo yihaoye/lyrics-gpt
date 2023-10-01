d8c574f11da2647894563323cf22ff8b753a9934
yihaoye <yihaoyemail@gmail.com>
2023-10-01 14:12:46 +1300

    param optimize

--- gpt.py
+++ gpt.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 16 # how many independent sequences will we process in parallel?
-block_size = 32 # what is the maximum context length for predictions?
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 64 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 100
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
-n_head = 4
-n_layer = 4
-dropout = 0.0
+n_embd = 128
+n_head = 6
+n_layer = 6
+dropout = 0.15
 # ------------
 
 torch.manual_seed(1337)
